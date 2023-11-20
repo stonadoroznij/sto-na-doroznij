260614ae142bdf40170af9842c98e1f5c97c94ee
Bohdan Leliuk <bohdan.leliuk@gmail.com>
2023-11-20 17:09:42 +0200

    add prettier config file and format all files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="max-w-352 m-auto p-6">
-      <section className="text-white">
-      <h2 className="font-title text-4xl md:text-5xl mb-6">Послуги</h2>
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-          <div className="w-full h-96 bg-coal-800 rounded-xl shadow-accent border border-accent-yellow" />
-          <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
-          <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
-          <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
-          <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
-          <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
-          <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
-          <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
-        </div>
-      </section>
-    </main>
-  );
+    return (
+        <main className="max-w-352 m-auto p-6">
+            <section className="text-white">
+                <h2 className="font-title text-4xl md:text-5xl mb-6">
+                    Послуги
+                </h2>
+                <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                    <div className="w-full h-96 bg-coal-800 rounded-xl shadow-accent border border-accent-yellow" />
+                    <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
+                    <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
+                    <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
+                    <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
+                    <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
+                    <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
+                    <div className="w-full h-96 bg-coal-800 rounded-xl shadow-dark border border-coal-600" />
+                </div>
+            </section>
+        </main>
+    )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,4 @@
-import Header from "./header/header";
-import Footer from "./footer/footer";
+import Header from './header/header'
+import Footer from './footer/footer'
 
-export {
-    Header,
-    Footer
-}
+export { Header, Footer }
