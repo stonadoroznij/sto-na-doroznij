04a044d1f8f874c57c3e41eca594bc1e726d3ed0
Bohdan Leliuk <bohdan.leliuk@gmail.com>
2023-11-21 13:23:17 +0200

    feat(tailwid.config): some little changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,36 +1,38 @@
-import type { Config } from 'tailwindcss'
-import defaultTheme from 'tailwindcss/defaultTheme';
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        "accent-yellow" : "#FCEA10",
-        "coal-800" : "#242424",
-        "coal-700" : "#3C3C3B",
-        "coal-600": "#484848"
+        "accent-yellow": "#FCEA10",
+        coal: {
+          "800": "#242424",
+          "700": "#3C3C3B",
+          "600": "#484848",
+        },
       },
-      fontFamily : {
+      fontFamily: {
         sans: ["'Montserrat'", ...defaultTheme.fontFamily.sans],
         title: ["'Russo One'"],
       },
       fontSize: {
-        '7xxl': '5rem'
+        "7xxl": "5rem",
       },
       boxShadow: {
-        'dark': '0px 4px 16px 0px rgba(0, 0, 0, 0.80)',
-        'accent': '0px 4px 16px 0px rgba(255, 224, 64, 0.20)',
+        dark: "0px 4px 16px 0px rgba(0, 0, 0, 0.80)",
+        accent: "0px 4px 16px 0px rgba(255, 224, 64, 0.20)",
       },
       maxWidth: {
-        '352': '88rem',
-      }
-    }
+        "352": "88rem",
+      },
+    },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
