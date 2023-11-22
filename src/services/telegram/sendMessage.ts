53a7cfa16091d255529f1275b7118e76d330be2a
Haifisch97 <72409661+Haifisch97@users.noreply.github.com>
2023-11-22 21:31:01 +0200

    refactor(sendmessage.ts): added check for undefind

--- src/services/telegram/sendMessage.ts
+++ src/services/telegram/sendMessage.ts
@@ -1,26 +1,26 @@
-import { Telegraf } from "telegraf";
-import { RequestMessage } from "../utils";
-import type { requestType } from "../../types";
-
-const TELEGRAM_BOT_TOKEN: string | undefined = process.env.TELEGRAM_BOT_TOKEN;
-const TELEGRAM_CHAT_ID: string | undefined = process.env.TELEGRAM_CHAT_ID;
-
-const bot = new Telegraf(TELEGRAM_BOT_TOKEN as string);
+import { Telegraf } from 'telegraf'
+import { RequestMessage } from '../utils'
+import type { requestType } from '../../types'
 
 const sendMessage = async (requestData: requestType) => {
-  try {
-    const requestMessage = new RequestMessage(requestData);
+    try {
+        const botToken: string | undefined = process.env.TELEGRAM_BOT_TOKEN
+        const chatId: string | undefined = process.env.TELEGRAM_CHAT_ID
 
-    await bot.telegram.sendMessage(
-      TELEGRAM_CHAT_ID as string,
-      requestMessage.markdown(),
-      {
-        parse_mode: "Markdown",
-      }
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
+        if (!botToken || !chatId) {
+            throw new Error('Telegram bot token or chat id is not defined')
+        }
 
-export default sendMessage;
+        const bot = new Telegraf(botToken)
+
+        const requestMessage = new RequestMessage(requestData)
+
+        await bot.telegram.sendMessage(chatId, requestMessage.markdown(), {
+            parse_mode: 'Markdown',
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default sendMessage
