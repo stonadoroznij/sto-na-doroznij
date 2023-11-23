a82cd16d17400949dd1a603384be8efac3088548
Bohdan Leliuk <bohdan.leliuk@gmail.com>
2023-11-23 17:20:40 +0200

    feat(smallform.tsx): implement zod schema to SmallForm

--- src/components/forms/smallform/SmallForm.tsx
+++ src/components/forms/smallform/SmallForm.tsx
@@ -1,12 +1,23 @@
 'use client'
 import { Button, TextInput } from '@/ui'
-import { useEffect, useState } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useState } from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
+import { z } from 'zod'
 
-interface IFormValues {
-    name: string
-    phone: number
-}
+const smallFromSchema = z.object({
+    name: z
+        .string()
+        .min(1, { message: "Ім'я обов'язкове поле!" })
+        .min(2, "Ім'я повинно мати більше 1 символу")
+        .trim(),
+    phone: z
+        .string({ required_error: "Телефон обо'язковий!" })
+        .min(1, { message: "Телефон обов'язкове поле!" })
+        .min(10, { message: 'Невірний телефон!' }),
+})
+
+type TSmallFormSchema = z.infer<typeof smallFromSchema>
 
 const SmallForm = () => {
     const [showSuccessfulMessage, setShowSuccessfulMessage] =
@@ -17,9 +28,11 @@ const SmallForm = () => {
         handleSubmit,
         reset,
         formState: { errors, isSubmitSuccessful },
-    } = useForm<IFormValues>()
+    } = useForm<TSmallFormSchema>({
+        resolver: zodResolver(smallFromSchema),
+    })
 
-    const onSubmit: SubmitHandler<IFormValues> = async (data) => {
+    const onSubmit: SubmitHandler<TSmallFormSchema> = async (data) => {
         const response = await new Promise((resolve) => {
             setTimeout(() => {
                 resolve(data)
@@ -37,26 +50,16 @@ const SmallForm = () => {
             className="flex flex-col gap-8 w-full pl-12 pr-12 lg:pl-0 lg:pr-0 lg:max-w-lg"
         >
             <div className="text-2xl font-semibold">Зв’язатися з нами</div>
-            <TextInput<IFormValues>
+            <TextInput<TSmallFormSchema>
                 placeholder="Iм'я"
                 label="name"
                 register={register}
-                options={{
-                    required: "Ім'я обо'язкове!",
-                    minLength: {
-                        value: 3,
-                        message: "Ім'я не може бути менше 3 символів",
-                    },
-                }}
                 error={errors.name?.message}
             />
-            <TextInput<IFormValues>
+            <TextInput<TSmallFormSchema>
                 placeholder="Телефон"
                 label="phone"
                 register={register}
-                options={{
-                    required: "Телефон обо'язковий!",
-                }}
                 error={errors.phone?.message}
             />
             <div className="relative flex justify-center">
