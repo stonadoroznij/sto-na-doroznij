7f19d3417561dc71f673561479578fb06dfb1cbd
Bohdan Leliuk <bohdan.leliuk@gmail.com>
2023-11-23 17:19:28 +0200

    fix(textinput.tsx): adapt TextInput for using with zod

--- src/ui/inputs/TextInput.tsx
+++ src/ui/inputs/TextInput.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import {
     FieldValues,
     Path,
-    RegisterOptions,
     UseFormRegister,
 } from 'react-hook-form'
 
 type InputProps<T extends FieldValues> = {
     label: Path<T>
     register: UseFormRegister<T>
-    options?: RegisterOptions<T, Path<T>>
     error?: string
     placeholder: string
 }
@@ -17,16 +15,15 @@ type InputProps<T extends FieldValues> = {
 const TextInput = function <T extends FieldValues>({
     label,
     register,
-    options,
     error,
     placeholder,
 }: InputProps<T>) {
     return (
         <div className="relative flex">
             <input
-                placeholder={`${options?.required ? '*' : ''}${placeholder}`}
+                placeholder={`${placeholder}`}
                 className="flex-1 bg-coal-800 bg-opacity-0 pb-2 outline-none border-b border-white focus:border-accent-yellow"
-                {...register(label, options)}
+                {...register(label)}
             />
             {error && (
                 <div className="flex-1 text-red-400 text-xs absolute -bottom-5 left-0">{`${error}`}</div>
