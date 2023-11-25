295b4a7c4ad279614088a36f14ed72480dd774f4
Bohdan Leliuk <bohdan.leliuk@gmail.com>
2023-11-25 14:23:10 +0200

    Versioning untracked files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Header from '../components/header/Header'
-import Footer from '../components/footer/Footer'
+import { Header, Footer } from '../components'
 
 const inter = Inter({ subsets: ['latin'] })
 
