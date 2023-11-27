7c92892b56e3f6e165e170cb99466e9019de3bb6
Bohdan Leliuk <bohdan.leliuk@gmail.com>
2023-11-27 13:22:21 +0200

    format

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import TikTokIcon from '../../../public/socialMediaIcons/TikTok.svg'
+import InstagramIcon from '../../../public/socialMediaIcons/Instagram.svg'
+import FacebookIcon from '../../../public/socialMediaIcons/Facebook.svg'
+import TelegramIcon from '../../../public/socialMediaIcons/Telegram.svg'
+import Image from 'next/image'
 
 const Contacts = () => {
+
   return (
     <main className="max-w-352 m-auto p-6">
       <section className="flex flex-col items-center gap-14 text-white">
@@ -12,7 +17,11 @@ const Contacts = () => {
           </div>
           <div className="flex flex-col gap-2 items-center lg:items-start">
             <h3 className="text-2xl font-semibold">Графік роботи</h3>
-            <div className="text-lg">Пн-Пт 9.00-18.00<br/>Сб 9.00-14.00</div>
+            <div className="text-lg">
+              Пн-Пт 9.00-18.00
+              <br />
+              Сб 9.00-14.00
+            </div>
           </div>
           <div className="flex flex-col gap-2 items-center lg:items-start">
             <h3 className="text-2xl font-semibold">Приїжайте</h3>
@@ -20,11 +29,16 @@ const Contacts = () => {
           </div>
           <div className="flex flex-col gap-2 items-center lg:items-start">
             <h3 className="text-2xl font-semibold">Слідкуйте</h3>
-            <div className="text-lg">Іконки</div>
+            <div className="flex gap-2 justify-start items-center flex-wrap">
+            <Image src={TikTokIcon} alt="icon tiktok" />
+            <Image src={InstagramIcon} alt="icon instagram" />
+            <Image src={FacebookIcon} alt="icon facebook" />
+            <Image src={TelegramIcon} alt="icon telegram" />
+          </div>
           </div>
         </div>
         <h2 className="mb-6 text-center">Побудувати маршрут в Google maps</h2>
-        <div className='w-64 h-64 bg-coal-600'/>
+        <div className="w-64 h-64 bg-coal-600" />
       </section>
     </main>
   )
