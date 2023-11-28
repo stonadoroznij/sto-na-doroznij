5a03cd2a3a8d1917dde797ec54a2e62e820f223d
Haifisch97 <72409661+Haifisch97@users.noreply.github.com>
2023-11-28 12:46:22 +0200

    refactor(client.ts): used the best practices

--- src/services/db/client.ts
+++ src/services/db/client.ts
@@ -1,11 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
-
-const prisma = global.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
+const prisma = new PrismaClient()
 
 export default prisma
