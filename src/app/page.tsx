c64cf95de18039e1aeca54751588bdb0674f6890
Kenmaqqe <50500934+kenmaqqe@users.noreply.github.com>
2023-11-30 17:30:03 +0200

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,19 @@ import Advantages6 from '../../public/AdvantagesIcons/Advantages-6.svg'
 export default function Home() {
   return (
     <main className="max-w-352 m-auto p-6 mt-60 flex-col flex content-between">
-      <Image src={BgImage} alt="фон"  className='shadow-2xl' placeholder="blur"
-      quality={100}
-      fill
-      sizes="100vw" style={{
-        objectFit: 'cover',
-        filter: "grayscale(50%)",
-        filter: "blur(2px) grayscale(50%) brightness(50%)",
-      }} />
+      <Image
+        src={BgImage}
+        alt="фон"
+        className="shadow-2xl"
+        placeholder="blur"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: 'cover',
+          filter: 'blur(2px) grayscale(50%) brightness(50%)',
+        }}
+      />
       <section className="text-white z-40 flex relative mb-14">
         <div className="w-1/2">
           <h1>
@@ -35,8 +40,8 @@ export default function Home() {
             незабутній клієнтський досвід, заснований на сімейних цінностях.
           </p>
           <div className="pt-8">
-            <Button >Зв’язатись з нами</Button>
-            <Button type='outline'>Замовити послугу</Button>
+            <Button>Зв’язатись з нами</Button>
+            <Button type="outline">Замовити послугу</Button>
           </div>
         </div>
       </section>
@@ -55,33 +60,52 @@ export default function Home() {
       <section className="font-bold font-serif flex flex-col z-20 mt-14 pt-14 text-white">
         <h2 className="flex justify-center">Переваги</h2>
         <div className="flex flex-wrap justify-center">
-          <AdvantagesCard imageSrc={Advantages1} text="Нове сучасне обладнання та інструмент."/>
-          <AdvantagesCard imageSrc={Advantages2} text="Досвідчені та кваліфіковані працівники."/>
-          <AdvantagesCard imageSrc={Advantages3} text="Широкий спектр послуг з ремонту та обслуговування авто."/>
-          <AdvantagesCard imageSrc={Advantages4} text="Гарантія на роботу та запчастини." />
-          <AdvantagesCard imageSrc={Advantages5} text="Сучасний 3D стенд розвал-сходження."/>
-          <AdvantagesCard imageSrc={Advantages6} text="Комфортна зона очікування для клієнтів."/>
+          <AdvantagesCard
+            imageSrc={Advantages1}
+            text="Нове сучасне обладнання та інструмент."
+          />
+          <AdvantagesCard
+            imageSrc={Advantages2}
+            text="Досвідчені та кваліфіковані працівники."
+          />
+          <AdvantagesCard
+            imageSrc={Advantages3}
+            text="Широкий спектр послуг з ремонту та обслуговування авто."
+          />
+          <AdvantagesCard
+            imageSrc={Advantages4}
+            text="Гарантія на роботу та запчастини."
+          />
+          <AdvantagesCard
+            imageSrc={Advantages5}
+            text="Сучасний 3D стенд розвал-сходження."
+          />
+          <AdvantagesCard
+            imageSrc={Advantages6}
+            text="Комфортна зона очікування для клієнтів."
+          />
         </div>
       </section>
       <section className="font-bold font-serif flex flex-col z-20 mt-14 pt-14 text-white">
-        <h2 className='flex justify-center'>Як відбувається процес</h2>
-        <div className='mt-6 mb-6 flex justify-center'>
-          <hr className='w-[56.25rem]'/>
+        <h2 className="flex justify-center">Як відбувається процес</h2>
+        <div className="mt-6 mb-6 flex justify-center">
+          <hr className="w-[56.25rem]" />
         </div>
-        <Button type='outline'>Зв'язатися з нами</Button>
+        <Button type="outline">Зв'язатися з нами</Button>
       </section>
       <section className="font-bold font-serif flex flex-col z-20 mt-14 pt-14 text-white">
         <div>
           <h2>Про нас</h2>
-          <p>“СТО на Дорожній” — це новий автосервіс, який заснований на засадах сімейного бізнесу. 
-            Молода команда професіоналів, які постійно розвиваються і впроваджують нові технології 
-            для досягнення найкращих результатів на всіх етапах виконання робіт.</p>
-          <Button type='outline'>Read more</Button>
+          <p>
+            “СТО на Дорожній” — це новий автосервіс, який заснований на засадах
+            сімейного бізнесу. Молода команда професіоналів, які постійно
+            розвиваються і впроваджують нові технології для досягнення найкращих
+            результатів на всіх етапах виконання робіт.
+          </p>
+          <Button type="outline">Read more</Button>
         </div>
         <div>
-          <Image 
-          src={AboutUs} 
-          alt='Про нас'/>
+          <Image src={AboutUs} alt="Про нас" />
         </div>
       </section>
       <section className="font-bold font-serif flex flex-col z-20 mt-14 pt-14 text-white">
--- src/components/advantagescard/AdvantagesCard.tsx
+++ src/components/advantagescard/AdvantagesCard.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const AdvantagesCard = (props) => {
   return (
     <div className=" rounded-lg border-solid border-black border-2 w-80 mr-6 flex items-center mt-6 p-6 shadow-dark">
-      <Image src={props.imageSrc} alt={props.imgAlt} className='pr-3'/>
+      <Image src={props.imageSrc} alt={props.imgAlt} className="pr-3" />
       <h3>{props.text}</h3>
     </div>
   )
