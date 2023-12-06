ea12b3cad7363fcd498100b98cf91e016fda82ee
Haifisch97 <72409661+Haifisch97@users.noreply.github.com>
2023-12-06 15:47:06 +0200

    Remove unused import statement

--- src/repository/request.repo.ts
+++ src/repository/request.repo.ts
@@ -1,6 +1,5 @@
 import { prisma } from '@/services/db'
 import { FormValues } from '@/schemas/zod-schemas'
-// import { serviceRepo } from './'
 
 class Request {
   public async add(formData: FormValues) {
