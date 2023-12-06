226d597946fdb616911bd8bc5424f3551f3b0851
Bohdan Leliuk <bohdan.leliuk@gmail.com>
2023-12-06 17:49:16 +0200

    style(aboutus): move all text to i18n

--- src/i18n/uk/index.ts
+++ src/i18n/uk/index.ts
@@ -2,5 +2,7 @@ import TelegramBot from './telegramBot'
 import Email from './Email'
 import Header from './Header'
 import Footer from './Footer'
+import AboutUs from './AboutUs'
+import ButtonText from './ButtonText'
 
-export { TelegramBot, Email, Header, Footer }
+export { TelegramBot, Email, Header, Footer, AboutUs, ButtonText }
