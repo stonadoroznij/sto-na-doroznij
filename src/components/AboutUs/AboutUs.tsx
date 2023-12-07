6c8a74aaee6cacb11ba7532320869c702e33fcd0
Kenmaqqe <50500934+kenmaqqe@users.noreply.github.com>
2023-12-07 17:25:10 +0200

    remake aboutUs

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import Image from 'next/image'
 import { Button } from '@/ui'
-import AboutUsImg from '../../../public/images/aboutus_2.png'
+import AboutUsImage2 from '../../../public/images/aboutus_2.png'
 
 const AboutUs = () => {
   return (
-    <div className="flex">
-      <div className="w-[26.25rem] h-[36rem] pt-[10.5rem] mr-[8.375rem]">
-        <h2 className="flex justify-start">Про нас</h2>
-        <p className="mt-[1.5rem] mb-[3rem] font-normal text-sm">
-          “СТО на Дорожній” — це новий автосервіс, який заснований на засадах
-          сімейного бізнесу. Молода команда професіоналів, які постійно
-          розвиваються і впроваджують нові технології для досягнення найкращих
-          результатів на всіх етапах виконання робіт.
-        </p>
-        <div className="flex justify-end">
-          <Button type="outline">Read more</Button>
-        </div>
-      </div>
-      <div>
-        <Image src={AboutUsImg} alt="Про нас" />
-      </div>
-    </div>
+    <section className="text-white flex flex-col-reverse justify-between items-center  gap-8 lg:flex-row">
+          <div className="flex flex-col gap-6 lg:w-[460px] lg:min-w-[460px]">
+            <h2>
+              Про нас
+            </h2>
+            <p>
+              <span className="font-bold">“СТО на Дорожній”</span> — це новий
+              автосервіс, який заснований на засадах сімейного бізнесу. Молода
+              команда професіоналів, які постійно розвиваються і впроваджують
+              нові технології для досягнення найкращих результатів на всіх
+              етапах виконання робіт.
+            </p>
+            <div className='flex justify-end'>
+              <Button type='outline'>Read more</Button>
+            </div>
+          </div>
+          <div className="max-w-[746px]">
+            <Image src={AboutUsImage2} alt="Фото нашого СТО всередині" />
+          </div>
+        </section>
   )
 }
 
