fa88b34f3c57caf2dd42f480eb63de9dc1c461e7
Haifisch97 <72409661+Haifisch97@users.noreply.github.com>
2023-12-22 16:02:03 +0200

    Add production server hostname to deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ APP_PATH="/srv/next-app"
 echo "Deploying to production server..."
 
 echo "${SERVER_HOSTNAME} is the hostname of the production server"
+
 # Decode SSH key
 echo "${SSH_KEY}" | base64 -d > ssh_key
 # private keys need to have strict permission to be accepted by SSH agent
