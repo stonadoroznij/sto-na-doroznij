058d3972928d9d029046de946ec180af05b40e03
Haifisch97 <72409661+Haifisch97@users.noreply.github.com>
2024-01-08 13:00:46 +0200

    Delete src/instrumentation.ts

--- src/instrumentation.ts
+++
@@ -1,7 +0,0 @@
-export async function register() {
-  console.log('NEXT_RUNTIME ', process.env.NEXT_RUNTIME)
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    console.log('register nodejs')
-    await import('@/services/telegram/initBot')
-  }
-}
